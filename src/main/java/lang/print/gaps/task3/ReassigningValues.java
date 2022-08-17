e351956178c806e2a8536da1d19a30cbe9d13f97
RemzerBens <111425570+RemzerBens@users.noreply.github.com>
2022-08-17 17:42:56 +0600

    Update ReassigningValues.java

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,9 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        
+        int first = 1, second = 10, third = 100;
+        
+        System.out.println(first);
     }
 }
